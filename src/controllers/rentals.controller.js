fe06fc4be62bd052754c32684a467b4b27721aaa
Andre Hilario <andre18hilario@gmail.com>
2023-05-09 14:21:24 -0300

    Feat: Rentals router

---
+++ src/controllers/rentals.controller.js
@@ -0,0 +1,17 @@
+
+
+export async function getRentals(req, res){
+
+}
+
+export async function postRentals(req, res){
+    
+}
+
+export async function postRentalsById(req, res){
+    
+}
+
+export async function deleteRentals(req, res){
+    
+}
