0a0f94a90f0aef9c42d8c8019c91a5fe44cc55b6
Andre Hilario <andre18hilario@gmail.com>
2023-05-11 09:00:20 -0300

    feat: post rentals controllers and schema

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -9,7 +9,28 @@ export async function postRentals(req, res) {
 
     const { customerId, gameId, daysRented } = req.body;
 
+    const date = dayjs();
+    const rentDate = date.format("YYYY-MM-DD");
+    const returnDate = null;
+    const delayFee = null;
+
     try {
+
+        const customerExists = await db.query(`SELECT * FROM customers WHERE id = $1;`, [customerId]);
+        if (customerExists.rows.length === 0) return res.sendStatus(400);
+
+
+        const gameExists = await db.query(`SELECT * FROM games WHERE id = $1;`, [gameId]);
+        if (gameExists.rows.length === 0) return res.sendStatus(400);
+
+
+        const priceDay = await db.query(`SELECT * FROM games WHERE id = $1;`, [gameId]);
+        const originalPrice = daysRented * (priceDay.rows[0].pricePerDay);
+
+        await db.query(`INSERT INTO rentals ("customerId", "gameId", "daysRented", "rentDate", "returnDate", "originalPrice", "delayFee") 
+            VALUES ($1, $2, $3, $4, $5, $6, $7);
+            `, [customerId, gameId, daysRented, rentDate, returnDate, originalPrice, delayFee]);
+
         res.sendStatus(201);
     } catch (err) {
         res.status(500).send(err.message);
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { deleteRentals, getRentals, postRentals, postRentalsById } from "../controllers/rentals.controller.js";
+import { validateSchema } from "../middlewares/validateSchema.middleware.js";
+import { rentalSchema } from "../schemas/rental.schema.js";
 
 
 const rentalsRouter = Router();
 
 rentalsRouter.get("/rentals", getRentals);
-rentalsRouter.post("/rentals", postRentals);
+rentalsRouter.post("/rentals", validateSchema(rentalSchema), postRentals);
 rentalsRouter.post("/rentals/:id/return", postRentalsById);
 rentalsRouter.delete("/rentals/:id", deleteRentals);
 
