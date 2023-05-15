8c61da16fa1dddef147061fdfe04a4c037879ae4
Andre Hilario <andre18hilario@gmail.com>
2023-05-15 14:45:47 -0300

    fix: birthday date, on get customers

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,39 +1,38 @@
 import { db } from "../database/database.js";
 
 export async function getCustomers(req, res) {
-
     const { cpf, order, desc } = req.query;
 
     try {
-
-        let customers;
         let queryString = `SELECT * FROM customers`;
+        let queryParams = [];
 
         if (cpf) {
             queryString += ` WHERE cpf LIKE $1`;
-            if (order) {
-                queryString += ` ORDER BY "${order}"`;
-                if (desc && desc.toLowerCase() === "true") {
-                    queryString += ` DESC`;
-                }
-            }
-            customers = await db.query(queryString, [`${cpf}%`]);
-        } else {
-            if (order) {
-                queryString += ` ORDER BY "${order}"`;
-                if (desc && desc.toLowerCase() === "true") {
-                    queryString += ` DESC`;
-                }
-            }
-            customers = await db.query(queryString);
+            queryParams.push(`${cpf}%`);
         }
+
+        if (order) {
+            queryString += ` ORDER BY ${order}`;
+            if (desc && desc.toLowerCase() === "true") {
+                queryString += ` DESC`;
+            }
+        }
+
+        let customers = await db.query(queryString, queryParams);
+
+
+        customers.rows = customers.rows.map(customer => {
+            const modifiedCustomer = { ...customer };
+            modifiedCustomer.birthday = new Date(modifiedCustomer.birthday).toISOString().split('T')[0];
+            return modifiedCustomer;
+        });
 
         res.send(customers.rows);
 
     } catch (err) {
         res.status(500).send(err.message);
     }
-
 };
 
 export async function getCustomersById(req, res) {
@@ -48,7 +47,11 @@ export async function getCustomersById(req, res) {
 
         if (customerById.rows.length === 0) return res.sendStatus(404);
 
-        res.send(customerById.rows[0]);
+        const modifiedCustomer = { ...customerById.rows[0] };
+        modifiedCustomer.birthday = new Date(modifiedCustomer.birthday).toISOString().split('T')[0];
+
+
+        res.send(modifiedCustomer);
 
     } catch (err) {
         res.status(500).send(err.message);
@@ -87,6 +90,7 @@ export async function editCustomers(req, res) {
 
         const existingCustomer = await db.query(`SELECT * FROM customers WHERE cpf = $1 AND id <> $2;`, [cpf, id]);
         if (existingCustomer.rowCount > 0) return res.sendStatus(409);
+
 
         await db.query(`UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;`, [name, phone, cpf, birthday, id]);
 
